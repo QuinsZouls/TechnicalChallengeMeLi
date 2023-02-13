d92ca025e6d7e6dfa8fcb99e278b8a7d1b0dd16b
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2023-02-13 11:00:43 -0600

    feature: add new api endpoints

--- service/src/bridges/ml.bridge.ts
+++ service/src/bridges/ml.bridge.ts
@@ -5,12 +5,20 @@ export default class MLBridge {
   private API = axios.create({
     baseURL: ML_API_SERVICE_URL,
   });
-  public async getItem(itemID: string, attributes: string[] = []) {
-    const params = new URLSearchParams();
-    if (attributes.length > 0) {
-      // Select specific attributes
-      params.set('attributes', attributes.toString());
-    }
-    return await this.API.get(`items/${itemID}?${params.toString()}`);
+  public async getItem<ItemType>(itemID: string, attributes: string[] = []): Promise<Awaited<ItemType>> {
+    const response = await this.API.get(`items/${itemID}?attributes=${attributes.toString()}`);
+    return response.data;
+  }
+  public async getCategory<ItemType>(categoryId: string, attributes: string[] = []): Promise<Awaited<ItemType>> {
+    const response = await this.API.get(`categories/${categoryId}?attributes=${attributes.toString()}`);
+    return response.data;
+  }
+  public async getCurrency<ItemType>(currencyId: string, attributes: string[] = []): Promise<Awaited<ItemType>> {
+    const response = await this.API.get(`currencies/${currencyId}?attributes=${attributes.toString()}`);
+    return response.data;
+  }
+  public async getUser<ItemType>(userId: string, attributes: string[] = []): Promise<Awaited<ItemType>> {
+    const response = await this.API.get(`users/${userId}?attributes=${attributes.toString()}`);
+    return response.data;
   }
 }
