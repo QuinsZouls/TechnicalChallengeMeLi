f827840074b802360e851920c763f3d0ea7aecda
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2023-02-17 23:20:58 -0600

    feature: implement object pool architecture

--- service/src/utils/queue.js
+++ service/src/utils/queue.js
@@ -8,12 +8,17 @@ class ProcessQueue extends EventEmitter {
   tasks = [];
   itemService = new ItemService();
   errors = 0;
+  #workers = [];
   constructor(max_process = 10) {
     super();
     this.MAX_PROCESSES = max_process;
     this.on('task_completed', () => {
       this.processTasks();
     });
+    /* Creating a new worker for each process that we want to run. */
+    for (let i = 0; i < max_process; i++) {
+      this.#workers.push(this.#setupWorker());
+    }
   }
   /**
    * If there are less than the maximum number of processes running, create a new child process and run
@@ -38,25 +43,59 @@ class ProcessQueue extends EventEmitter {
       this.createChildProcess(this.tasks.shift());
     }
     if (this.activeProcesses === 0) {
+      // Finish queue
       this.emit('done');
     }
   }
   /* Creating a child process and sending a task to it. */
   createChildProcess(task) {
     this.activeProcesses++;
-    const worker = new Worker('./src/workers/items.worker.js', {
-      workerData: task,
-    });
-    worker.on('message', async msg => {
-      await this.itemService.createItem(msg);
-    });
-    worker.on('exit', e => {
-      this.activeProcesses--;
-      if (e === -1) {
+    const worker = this.#getFirstFreeWorker();
+    worker.active = true;
+    worker.instance.postMessage(task);
+  }
+  /**
+   * It creates a new worker instance, and returns an object with the worker instance and a boolean
+   * value indicating whether the worker is active or not
+   * @returns A worker object with an instance of a web worker and a boolean value for active.
+   */
+  #setupWorker() {
+    const worker = {
+      active: false,
+      instance: new Worker('./src/workers/items.worker.js', {
+        workerData: this.itemService,
+      }),
+    };
+    worker.instance.on('message', async response => {
+      if (!response.status) {
         this.errors++;
+      } else {
+        this.itemService.createItem(response.data);
       }
+      this.activeProcesses--;
+      worker.active = false;
       this.emit('task_completed');
     });
+    return worker;
+  }
+  /**
+   * "Return the first worker that is not active, or create a new worker if all workers are active."
+   *
+   * The first thing we do is loop through all of the workers in the `workers` array. If we find a
+   * worker that is not active, we return it. If we don't find a worker that is not active, we create a
+   * new worker and return it
+   * @returns A worker that is not active.
+   */
+  #getFirstFreeWorker() {
+    for (const worker of this.#workers) {
+      if (!worker.active) {
+        return worker;
+      }
+    }
+    // Create worker if its necessary
+    const newWorker = this.#setupWorker();
+    this.#workers.push(newWorker);
+    return newWorker;
   }
 }
 
--- service/src/workers/items.worker.js
+++ service/src/workers/items.worker.js
@@ -1,33 +1,37 @@
-const { workerData, parentPort, isMainThread } = require('worker_threads');
+const { parentPort, isMainThread } = require('worker_threads');
 const MLBridge = require('../bridges/ml.bridge');
-// Listen messages
+
 const processData = async msg => {
   const API = new MLBridge();
-  try {
-    const item = await API.getItem(msg.site + msg.id, ['price', 'start_time', 'category_id', 'currency_id', 'seller_id']);
-    const [category, currency, seller] = await Promise.all([
-      API.getCategory(item.category_id, ['name']),
-      API.getCurrency(item.currency_id, ['description']),
-      API.getUser(item.seller_id.toString(), ['nickname']),
-    ]);
-    const record = {
-      site: msg.site,
-      id: msg.id,
-      price: item.price,
-      start_time: item.start_time,
-      name: category.name,
-      description: currency.description,
-      nickname: seller.nickname,
-    };
-    if (process.send) {
-      process?.send(record);
-    }
-    parentPort.postMessage(record);
-    process.exit(0);
-  } catch (error) {
-    process.exit(-1);
-  }
+  const item = await API.getItem(msg.site + msg.id, ['price', 'start_time', 'category_id', 'currency_id', 'seller_id']);
+  const [category, currency, seller] = await Promise.all([
+    API.getCategory(item.category_id, ['name']),
+    API.getCurrency(item.currency_id, ['description']),
+    API.getUser(item.seller_id.toString(), ['nickname']),
+  ]);
+  const record = {
+    site: msg.site,
+    id: msg.id,
+    price: item.price,
+    start_time: item.start_time,
+    name: category.name,
+    description: currency.description,
+    nickname: seller.nickname,
+  };
+  parentPort.postMessage({
+    status: 1,
+    data: record,
+  });
 };
 if (!isMainThread) {
-  processData(workerData);
+  parentPort.on('message', async row => {
+    try {
+      await processData(row);
+      // Send success status
+    } catch (error) {
+      parentPort.postMessage({
+        status: 0,
+      });
+    }
+  });
 }
